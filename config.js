78c7b0215bb7cd7089b929d85e50fc1558acb3d1
Oliver Foster <oliver.foster@kineo.com>
2017-10-09 17:55:52 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+require.config({
+  map: {
+    '*': {
+      'core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView',
+      'adapt-contrib-core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView'
+    }
+  }
+});
---
+++ js/navigation.js
@@ -0,0 +1,20 @@
+define([
+    'adapt-contrib-core/js/adapt',
+    './views/navigationView'
+], function(Adapt, NavigationView) {
+
+    require.config({
+      map: {
+        '*': {
+          'core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView',
+          'adapt-contrib-core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView'
+        }
+      }
+    });
+
+    Adapt.on("navigation:initialize", function() {
+        Adapt.navigation = new NavigationView();
+    });
+
+
+});
