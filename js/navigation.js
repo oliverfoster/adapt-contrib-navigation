32ed723ce3685b8f63490848e5a60ea66bddb0f7
Oliver Foster <oliver.foster@kineo.com>
2017-10-10 08:45:30 +0100

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -6,8 +6,8 @@ define([
     require.config({
       map: {
         '*': {
-          'core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView',
-          'adapt-contrib-core/js/views/navigationView': 'adapt-contrib-notify/js/views/navigationView'
+          'core/js/views/navigationView': 'adapt-contrib-navigation/js/views/navigationView',
+          'adapt-contrib-core/js/views/navigationView': 'adapt-contrib-navigation/js/views/navigationView'
         }
       }
     });
